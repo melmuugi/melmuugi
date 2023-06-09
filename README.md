4fa0dc16b98ee085d6529acc85c5792e1885601d
melmuugi <melmuugi@gmail.com>
2023-06-10 01:49:14 +0300

    new file:   .eslintrc.json new file:   .gitignore new file:   .vercelignore new file:   .vscode/settings.json new file:   LICENSE new file:   README.md new file:   app/head.tsx new file:   app/layout.tsx new file:   app/page.tsx new file:   components/AboutSection.tsx new file:   components/ContactSection.tsx new file:   components/Footer.tsx new file:   components/HeroSection.tsx new file:   components/Navbar.tsx new file:   components/ProjectsSection.tsx new file:   components/SlideUp.tsx new file:   demo/demo.gif new file:   next.config.js new file:   package.json new file:   pages/api/hello.ts new file:   postcss.config.js new file:   public/Charmaines.jpg new file:   public/about.png new file:   public/headshot.jpg new file:   public/logoo.png new file:   public/ocr.jpg new file:   public/portfolio.jpg new file:   styles/globals.css new file:   tailwind.config.js new file:   tsconfig.json

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tailwind CSS and Next.js Portfolio
+![Portfolio Demo](demo/demo.gif)
+
+To Run, in Terminal type:
+
+npm install
+npm run dev
---
+++ components/ContactSection.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import SlideUp from "./SlideUp"
+import {
+    AiOutlineGithub,
+    AiOutlineTwitter,
+    AiOutlineLinkedin,
+    AiOutlineYoutube,
+    AiOutlinePhone,
+  } from "react-icons/ai"
+
+
+const ContactSection = () => {
+  return (
+        <div className="my-12 pb-12 md:pt-16 md:pb-45"> 
+        <h1 className="text-center font-bold text-4xl">
+          Get In Touch
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+          </h1>
+          <div className="flex flex-col justify-between justify-center items-center animate-slideUpCubiBezier animation-delay-6 my-12 pb-12 md:pt-16 md:pb-25">
+            <div className="border-3 w-full max-w-2xl space-y-8 p-8 rounded-lg shadow-lg">
+            <div className="flex flex-row items-center justify-center space-x-1 mb-1 text-xl">    
+            <AiOutlinePhone className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}/>
+              <span> +254 704 690042</span>
+            </div>
+            <div className="flex flex-row items-center justify-center space-x-2 mb-1">
+          <a href="https://github.com/melmuugi" rel="noreferrer" target="_blank">
+            <AiOutlineGithub
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://twitter.com/melmuugi"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineTwitter
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/melmuugi/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/watch?v=tXyx3E81Bbo"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineYoutube
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+        </div>
+            </div>
+            </div>
+        </div>
+  )
+}
+
+export default ContactSection
